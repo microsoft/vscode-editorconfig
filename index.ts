6a0b7e5042da882d70c6db4f793242612d7037b4
Chris Dias <chris@diasfam.com>
2015-09-17 18:30:32 +0200

    start now works

--- index.ts
+++ index.ts
@@ -1,114 +1,163 @@
 import * as vscode from 'vscode';
 import * as editorconfig from 'editorconfig';
-import * as vscode2 from 'vscode2';
 import * as fs from 'fs';
+
 
 export function activate() {
     
     // hook into the "editor open" event and apply .editorconfig files on that document
     // i want this to apply for all open editors as well as any new editors, in case my extension 
     // is loaded while i have open files
-    
-    vscode2.shell.onEditorOpen(editor => {
-        applyEditorConfig(editor);
-    })    
-    
+    vscode.commands.registerCommand('vscode.editorconfig', startEditorConfig);
+
     // register a command handler to generatoe a .editorconfig file
     vscode.commands.registerCommand('vscode.generateeditorconfig', generateEditorConfig);
 }
 
-function applyEditorConfig(e: vscode2.Editor) {
+function startEditorConfig() {
+    let editor_tabSize:any;
+    let editor_insertSpaces:any;
 
-    // get the file path to pass to the editorconfig module
-    const doc: vscode2.Document = e.getDocument();
-    const filepath: string = doc.getUri().path;
-    
-    // check to see if this is an empty, no name file
-    if (filepath === '') {
-        return;
-    }
-
-    editorconfig
-        .parse(filepath)
-        .then(config => {
-
-            // check to see if we found a .editorconfig
-            if (Object.keys(config).length === 0) {
-                return;
-            }
+    vscode.shell.onEditorOpen(e => {
+        
+        let subscription = e.onDocumentChange(function() {
             
-            // we did, so set the properties
-            const indentStyle = config.indent_style || (doc.insertSpaces() ? 'space' : 'tab');
+            const doc: vscode.Document = e.getDocument();
+            const filepath: string = doc.getUri().fsPath;
 
-            if (indentStyle === 'tab') {
-                doc.insertSpaces = false;
-                if (config.indent_size) {
-                    doc.setTabSize = config.indent_size;
-                }
-            }
+            // the editorconfig module will scan the folder and parent folders to build
+            // an editorconfig property bag for us
+            editorconfig
+                .parse(filepath)
+                .then(config => {
 
-            if (config.charset) {
-                doc.setEncoding(config.charset);
-            }
+                    // check to see if we found an .editorconfig file
+                    if (Object.keys(config).length === 0) {
+                        return;
+                    }
+                    
+                    // get our settings for insertSpaces and tabSize (all i can get right now)
+                    vscode.Services.ConfigurationService.loadConfiguration("editor").then(value => {
+                        editor_insertSpaces = value.insertSpaces;  // auto, true, false
+                        editor_tabSize = value.tabSize; //auto, 2, 4, 6, ...
+                    
+                        // initialize our editor options with the current settings                     
+                        let opts:vscode.EditorOptions = {useSpaces: editor_insertSpaces, 
+                                                           tabSize: editor_tabSize};
+                                                           
+                        // if editorconfig properties exist, use them otherwise keep defaults
+                        // useSpaces must be converted 'tab' -> false, 'space' -> true
+                        opts.useSpaces = config.indent_style ? (config.indent_style === 'tab' ? false : true) : editor_insertSpaces;
+                        opts.tabSize = config.indent_size ? config.indent_size : editor_tabSize;
+                        
+                        // TODO: set these properties when they become available!                        
+                        // config.charset
+                        // config.end_of_line
+                        // config.trim_trailing_whitespace
+                                                
+                        // update our editor
+                        e.updateOptions(opts);
 
-            if (config.end_of_line) {
-                doc.setEndOfLine = config.end_of_line;
-            }
+                    });
 
-            if (config.trim_trailing_whitespace) {
-                doc.trimTrailingWhitespace = config.trim_trailing_whitespace;
-            }
-
+                });
         });
+        
+        // clean up
+        //subscription.dispose();
+        
+    });
+    
 }
 
 function generateEditorConfig() {
     // generate a .editorconfig file in the root of the workspace
     // based on the current editor settings and buffer properties
    
-    const configFile: string = vscode.workspace.getPath() + '.editorconfig';
-
-    const configSvcs = {
-        editor: vscode.Services.Configuration.get("editor");
-        core: vscode.Services.Configuration.get("core");
-    }
-
-    // pull settings from configuration service
-    const indent_style = configSvcs.editor.insertTabs() ? 'tab' : 'space';
-    const indent_size = configSvcs.editor.tabSize();
-    const end_of_line = configSvcs.editor.eol();
-    const charset = configSvcs.core.charset();
-    const trim_trailing_whitespace = configSvcs.core.trimTrailingWhitespace();
-    const insert_final_newline = configSvcs.core.insertFinalNewline();
+   if (vscode.workspace.getPath() === null) {
+       vscode.shell.showInformationMessage("Please open a folder before generating an .editorconfig file");
+       return;
+   }
    
-    // atom pattern
-    const fileContents = `
-        root = true
+   const configFile: string = vscode.workspace.getPath() + '/.editorconfig';
 
-        [*]
-        indent_style = ${indent_style}
-        indent_size = ${indent_size}
-        end_of_line = ${end_of_line}
-        charset = ${charset}
-        trim_trailing_whitespace = ${trim_trailing_whitespace}
-        insert_final_newline = ${insert_final_newline}
-        `;
+   fs.exists(configFile, exists => {
+       if (exists) {
+           vscode.shell.showInformationMessage("An .editorconfig file already exists in this folder.");
+           return;
+       }
+       
+       
+   });
+   
+                       // get our settings for insertSpaces and tabSize (all i can get right now)
+                    // vscode.Services.ConfigurationService.loadConfiguration("editor").then(value => {
+                    //     editor_insertSpaces = value.insertSpaces;  // auto, true, false
+                    //     editor_tabSize = value.tabSize; //auto, 2, 4, 6, ...
+                    
+                    // // is this right?
+                    // }).then(value => {
+                        
+                    //     // initialize our editor options with the current settings                     
+                    //     let opts:vscode.EditorOptions = {useSpaces: editor_insertSpaces, 
+                    //                                        tabSize: editor_tabSize};
+                                                           
+                    //     // if editorconfig properties exist, use them otherwise keep defaults
+                    //     // useSpaces must be converted 'tab' -> false, 'space' -> true
+                    //     opts.useSpaces = config.indent_style ? (config.indent_style === 'tab' ? false : true) : editor_insertSpaces;
+                    //     opts.tabSize = config.indent_size ? config.indent_size : editor_tabSize;
+                        
+                    //     // TODO: set these properties when they become available!                        
+                    //     // config.charset
+                    //     // config.end_of_line
+                    //     // config.trim_trailing_whitespace
+                                                
+                    //     // update our editor
+                    //     e.updateOptions(opts);
 
-    fs.access(configFile, err => {
-        if (err) {
-            fs.writeFile(configFile, fileContents, err => {
-                if (err) {
-                    vscode.notifications.addError(err);
-                    return;
-                }
+                    });
 
-                vscode.notifications.addSuccess('.editorconfig file successfully generated', {
-                    detail: 'An .editorconfig file was successfully generated in your project based on your current settings.'
-                });
-            });
-        } else {
-            vscode.notifications.addError('An .editorconfig file already exists in your workspace root.');
-        }
-    });
+    // const configSvcs = {
+    //     editor: vscode.Services.Configuration.get("editor");
+    //     core: vscode.Services.Configuration.get("core");
+    // }
+
+    // // pull settings from configuration service
+    // const indent_style = configSvcs.editor.insertTabs() ? 'tab' : 'space';
+    // const indent_size = configSvcs.editor.tabSize();
+    // const end_of_line = configSvcs.editor.eol();
+    // const charset = configSvcs.core.charset();
+    // const trim_trailing_whitespace = configSvcs.core.trimTrailingWhitespace();
+    // const insert_final_newline = configSvcs.core.insertFinalNewline();
+   
+    // // atom pattern
+    // const fileContents = `
+    //     root = true
+
+    //     [*]
+    //     indent_style = ${indent_style}
+    //     indent_size = ${indent_size}
+    //     end_of_line = ${end_of_line}
+    //     charset = ${charset}
+    //     trim_trailing_whitespace = ${trim_trailing_whitespace}
+    //     insert_final_newline = ${insert_final_newline}
+    //     `;
+
+    // fs.access(configFile, err => {
+    //     if (err) {
+    //         fs.writeFile(configFile, fileContents, err => {
+    //             if (err) {
+    //                 vscode.notifications.addError(err);
+    //                 return;
+    //             }
+
+    //             vscode.notifications.addSuccess('.editorconfig file successfully generated', {
+    //                 detail: 'An .editorconfig file was successfully generated in your project based on your current settings.'
+    //             });
+    //         });
+    //     } else {
+    //         vscode.notifications.addError('An .editorconfig file already exists in your workspace root.');
+    //     }
+    // });
 }
 
