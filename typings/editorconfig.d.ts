160851cb5b7e0d4affeb1b273919a831d20e8460
Chris Dias <chris@diasfam.com>
2015-11-25 09:34:59 -0800

    Complete fix for indent_size being returned as "tab"

--- typings/editorconfig.d.ts
+++ typings/editorconfig.d.ts
@@ -3,7 +3,7 @@ declare module 'editorconfig' {
     export interface knownProps {
         end_of_line?: string,
         indent_style?: string,
-        indent_size?: number,
+        indent_size?: any,
         insert_final_newline?: boolean,
         trim_trailing_whitespace?: boolean,
         charset?: string
